8b3dfa3bbb776b194bc7a1c70b4172078cf1fcb3
j <9ingpictures@gmail.com>
2024-04-18 18:08:30 +0900

    add api folder

--- test/src/views/NewsView.vue
+++ test/src/views/NewsView.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <div v-for="(user, index) in users" :key="index">{{key}}{{ index }}</div>
+    <div v-for="(user, index) in users" :key="index">{{ user }}</div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
+import { fetchNewsList } from '../api/index.js';
 
 export default {
   data() {
@@ -14,9 +15,12 @@ export default {
     }
   },
   created() {
-    axios.get('https://api.hnpwa.com/v0/news/1.json')
+    var vm = this;
+    // axios.get('https://api.hnpwa.com/v0/news/1.json')
+    fetchNewsList()
       .then(function(response) {
         console.log(response)
+        vm.users = response.data
       })
       .catch(function(error) {
         console.log(error);
