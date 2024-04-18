e1acbd5ad5e77c9f0849daaf53b425110f8a0eb1
j <9ingpictures@gmail.com>
2024-04-18 16:35:17 +0900

    new test project create

---
+++ test/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <top-area></top-area>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TopArea from './components/TopArea.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TopArea,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ test/src/views/NewsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="(user, index) in users" :key="index">{{key}}{{ index }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: [],
+    }
+  },
+  created() {
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(function(response) {
+        console.log(response)
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
