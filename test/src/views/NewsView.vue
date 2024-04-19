e5c9a758037c97309971338a4ab6a9beac3148fb
j <9ingpictures@gmail.com>
2024-04-19 14:38:42 +0900

    vuex

--- test/src/views/NewsView.vue
+++ test/src/views/NewsView.vue
@@ -1,32 +1,38 @@
 <template>
   <div>
-    <div v-for="(user, index) in users" :key="index">{{ user.title }}</div>
+    <div v-for="(user, index) in this.$store.state.news" :key="index">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
 // import axios from 'axios';
-import { fetchNewsList } from '../api/index.js';
+// import { fetchNewsList } from '../api/index.js';
 
 export default {
-  data() {
-    return {
-      users: [],
-    }
-  },
+  // data() {
+  //   return {
+  //     users: [],
+  //   }
+  // },
   created() {
     console.log('호출 전: ', this);
     // var vm = this;
     // axios.get('https://api.hnpwa.com/v0/news/1.json')
-    fetchNewsList()
-      .then(function(response) {
-        console.log('호출 후: ', this);
-        console.log('response:', response)
-        // vm.users = response.data
-      })
-      .catch(function(error) {
-        console.log(error);
-      })
+    // 1
+    // fetchNewsList()
+    //   // .then(function(response) {
+    //   //   console.log('호출 후: ', this);
+    //   //   console.log('response:', response)
+    //   //   // vm.users = response.data
+        
+    //   // })
+    //   // 2
+    //   .then(response => this.users = response.data)
+    //   // .catch(function(error) {
+    //   //   console.log(error);
+    //   // })
+    //   .catch(error => console.log(error));
+    this.$store.dispatch('FETCH_NEWS');
   }
 }
 </script>
