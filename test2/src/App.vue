0f638d250219996619fdf6f6ea265d517d94e056
keep9ing <9ingpictures@gmail.com>
2024-04-27 02:39:14 +0900

    test2 setting

--- test2/src/App.vue
+++ test2/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <app-header></app-header>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from './components/AppHeader.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
+    AppHeader,
   },
 };
 </script>
