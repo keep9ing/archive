353ddb436ffd1d5f7b0fa50f3535398a17895f8d
j <9ingpictures@gmail.com>
2024-05-02 14:40:26 +0900

    vuetify add

--- test2/src/main.js
+++ test2/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from '@/routes/index';
 import store from '@/store/index';
 import { formatDate } from '@/utils/filters';
+import vuetify from './plugins/vuetify';
 
 Vue.filter('formatDate', formatDate);
 Vue.config.productionTip = false;
@@ -10,5 +11,6 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
   router,
+  vuetify,
   store,
 }).$mount('#app');
