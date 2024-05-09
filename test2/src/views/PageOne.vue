41ca98eddb73af3abdc360563e729c3f1bcad370
j <9ingpictures@gmail.com>
2024-05-09 11:22:06 +0900

    jsonplaceholder posts

--- test2/src/views/PageOne.vue
+++ test2/src/views/PageOne.vue
@@ -2,14 +2,18 @@
   <div>
     <v-app>
       <v-container>
-        <v-row>
-          <v-col v-for="n in 3" :key="n" cols="12" sm="4">
-            <v-sheet> One of three columns </v-sheet>
+        <v-row v-for="(item, index) of list" :key="index">
+          <v-col cols="12">
+            <!-- <v-sheet> One of three columns </v-sheet> -->
+            {{ item.id }}
+          </v-col>
+          <v-col cols="12" sm="4">
+            {{ item.title }}
           </v-col>
         </v-row>
         <v-btn outlined depressed rounded @click="save()">저장</v-btn>
         <v-btn outlined depressed rounded @click="close()">닫기</v-btn>
-        <v-simple-table>
+        <v-table density="compact">
           <tbody>
             <tr>
               1
@@ -32,20 +36,43 @@
               </td>
             </tr>
           </tbody>
-        </v-simple-table>
+        </v-table>
       </v-container>
     </v-app>
   </div>
 </template>
 
 <script>
+import test from '@/api/test';
+
 export default {
   data() {
     return {
       selectedItem: '',
       radioValue: '',
       items: ['California', 'Colorado', 'Florida'],
+      list: [],
     };
+  },
+  created() {
+    test
+      .getArticle(1)
+      .then(response => console.log('getAritcle', response))
+      .catch(error => console.log(error));
+
+    test
+      .getArticles(0)
+      .then(response => {
+        console.log('getAritcles', response);
+        this.list = response.data;
+        console.log(this.list);
+      })
+      .catch(error => console.log(error));
+
+    test
+      .postArticle('id', 'title', 'body')
+      .then(response => console.log('postArticle', response))
+      .catch(error => console.log(error));
   },
 };
 </script>
