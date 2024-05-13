fabab31b75d0170cdb8603a481d4950994317b23
j <9ingpictures@gmail.com>
2024-05-13 22:58:51 +0900

    v-data-table

--- test2/src/views/PageOne.vue
+++ test2/src/views/PageOne.vue
@@ -4,39 +4,46 @@
       <v-container>
         <v-btn outlined depressed rounded @click="save()">저장</v-btn>
         <v-btn outlined depressed rounded @click="close()">닫기</v-btn>
+        <v-table>
+          <tr>
+            <td>
+              <v-select
+                :items="items"
+                density="comfortable"
+                label="지역"
+              ></v-select>
+            </td>
+          </tr>
+
+          <tr>
+            <!-- <v-radio-group v-model="radioValue">
+              <v-radio label="무료" value="n"></v-radio>
+              <v-radio label="유료" value="y"></v-radio>
+            </v-radio-group> -->
+            <td>
+              <v-text-field outlined solo flat hide-details>{{}}</v-text-field>
+            </td>
+          </tr>
+        </v-table>
+
         <v-text-field v-model="search" single-line></v-text-field>
         <v-data-table
-          :items-per-page="5"
+          :items-per-page="10"
           :search="search"
           :items="list"
+          :item-key="index"
           :headers="headers"
+          @click:row="item => goDetail(item.id)"
         >
-          <tbody>
-            <tr>
-              <td v-for="(item, index) of list" :key="index">
-                {{ item.id }} , {{ item.title }}
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <v-select
-                  :items="items"
-                  density="comfortable"
-                  label="지역"
-                ></v-select>
-              </td>
-            </tr>
+          <template v-slot:no-data>
+            <v-alert :value="true" color="error" icon="warning">
+              Sorry, nothing to display here
+            </v-alert>
+          </template>
 
-            <tr>
-              <v-radio-group v-model="radioValue">
-                <v-radio label="무료" value="n"></v-radio>
-                <v-radio label="유료" value="y"></v-radio>
-              </v-radio-group>
-              <td>
-                <v-text-field outlined solo flat hide-details></v-text-field>
-              </td>
-            </tr>
-          </tbody>
+          <template v-slot:detail-page>
+            <Page-Detail></Page-Detail>
+          </template>
         </v-data-table>
       </v-container>
     </v-app>
@@ -44,9 +51,13 @@
 </template>
 
 <script>
+import PageDetail from './PageDetail.vue';
 import test from '@/api/test';
 
 export default {
+  components: {
+    PageDetail,
+  },
   data() {
     return {
       search: '',
@@ -57,7 +68,7 @@ export default {
       headers: [
         { text: 'id', value: 'id' },
         { text: 'title', value: 'title' },
-        { text: 'body', value: 'body' },
+        // { text: 'body', value: 'body' },
       ],
     };
   },
@@ -81,10 +92,15 @@ export default {
       .then(response => console.log('postArticle', response))
       .catch(error => console.log(error));
 
-    test
-      .getEmoji('emoji')
-      .then(response => console.log('getEmoji', response))
-      .catch(error => console.log(error));
+    // test
+    //   .getEmoji('emoji')
+    //   .then(response => console.log('getEmoji', response.data))
+    //   .catch(error => console.log(error));
+  },
+  methods: {
+    goDetail(id) {
+      this.$router.push(`/page1/${id}`);
+    },
   },
 };
 </script>
